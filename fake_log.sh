b6e0368c19cbf22f5583ea88136335e99aec737c
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2023-10-20 20:59:48 +0800

    Delete fake_log.sh

--- fake_log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TOOL_PATH="poetry run csv2notion_neo"
-NOTION_TOKEN="v02%3Auser_token_or_cookies%3Ac1TyK1LD5XpYfGLqxmDlJsN_PB-AGy9EBmnIMxMgQy7JNfETpdqgC2mK_OFdSF6gifcZiEBU53Xk3QuHQlBstsftZNtV3jzcgoXhtGX7FRIhxq1ravBTc9DW1f--Fr7DhYmZ"
-NOTION_URL="https://www.notion.so/9275939ac1cd4214a44a54ec6da27383?v=3610508219144eabbe9fc846383920a1&pvs=4"
-UPLOAD_CSV="fake_file.csv"
-UPLOAD_LOG="test_log.txt"
-
-$TOOL_PATH --token $NOTION_TOKEN --url $NOTION_URL --log $UPLOAD_LOG --verbose "$UPLOAD_CSV"
