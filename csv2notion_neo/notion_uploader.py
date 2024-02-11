5bbc67fab839a0c9baff476e15aea5d5f220238d
Arjun <arjunprakash027@gmail.com>
2024-02-11 16:59:50 +0530

    fix: added check for image before getting cover image

--- csv2notion_neo/notion_uploader.py
+++ csv2notion_neo/notion_uploader.py
@@ -12,9 +12,10 @@ class NotionUploadRow(object):
     properties: Dict[str, Any]
 
     def key(self) -> str:
-    
-        if self.properties["payload_key_column"]:
-            return str(self.columns[self.properties["payload_key_column"]])
+        
+        if "payload_key_column" in self.properties:
+            if self.properties["payload_key_column"]:
+                return str(self.columns[self.properties["payload_key_column"]])
         return str(list(self.columns.values())[0])
 
 
@@ -35,20 +36,13 @@ class NotionRowUploader(object):
     def _get_db_row(
         self, row: NotionUploadRow, is_merge: bool
     ) -> CollectionRowBlockExtended:
-
       
         existing_row = self.db.rows.get(row.key()) if is_merge else None
-
-
-        
-        if is_merge and existing_row:
-       
+        if is_merge and existing_row:       
             cur_row = existing_row
             cur_row.update(properties=row.properties, columns=row.columns)
         else:
-         
             cur_row = self.db.add_row(properties=row.properties, columns=row.columns)
-
         return cur_row
 
 
