aa99d5122a0c6520102c809fbaca9be9fab4a13f
Arjun prakash Rao <arjunprakashrao@Arjuns-MacBook-Air.local>
2025-02-15 19:56:01 +0530

    added dockerfile

---
+++ install_from_docker.sh
@@ -0,0 +1,8 @@
+IMAGE_NAME="neo_python:latest"
+CONTAINER_NAME="neo_python_container"
+
+# Building the image
+docker buildx bake
+
+# Creating a container from that image and running it 
+docker run --name $CONTAINER_NAME -d --memory="512m" --cpus="1.0" -p 2222:22 $IMAGE_NAME
