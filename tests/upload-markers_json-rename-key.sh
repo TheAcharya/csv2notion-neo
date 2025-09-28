9ccc63d6bcab07b4b71e88dc61fdf6a13152bd69
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2025-09-28 09:09:08 +0800

    Update Upload Test

--- tests/upload-markers_json-rename-key.sh
+++ tests/upload-markers_json-rename-key.sh
@@ -13,4 +13,4 @@ UPLOAD_PAYLOAD="tests/assets/notion-upload-test.json"
 UPLOAD_LOG="tests/log.txt"
 
 #$TOOL_PATH --workspace $NOTION_WORKSPACE --token $NOTION_TOKEN --url $NOTION_URL --image-column "Image Filename" --image-column-keep --mandatory-column "Marker ID" --icon-column "Icon Image" --payload-key-column "Marker ID" --max-threads 5 --merge --log $UPLOAD_LOG --verbose $UPLOAD_PAYLOAD
-$TOOL_PATH --workspace "$NOTION_WORKSPACE" --token $NOTION_TOKEN --url $NOTION_URL --randomize-select-colors --image-column "Image Filename" --randomize-select-colors --image-column-keep --mandatory-column "Marker ID" --payload-key-column "Marker ID" --merge  --max-threads 5 --max-threads 1  --log "$UPLOAD_LOG" --verbose "$UPLOAD_PAYLOAD"
+$TOOL_PATH --workspace "$NOTION_WORKSPACE" --token $NOTION_TOKEN --url $NOTION_URL --randomize-select-colors --image-column "Image Filename" --randomize-select-colors --image-column-keep --mandatory-column "Marker ID" --icon-column "Icon Image" --payload-key-column "Marker ID" --merge  --max-threads 5 --max-threads 1  --log "$UPLOAD_LOG" --verbose "$UPLOAD_PAYLOAD"
